1f9ad837530893c8c8d69812b9cff28065a1cc60
Hideaki Tai <hideaki.tai@gmail.com>
2020-05-13 15:58:36 +0900

    improve support for no-stl boards

--- MsgPacketizer.h
+++ MsgPacketizer.h
@@ -23,76 +23,152 @@ namespace msgpacketizer {
     using StreamType = ofSerial;
 #endif
 
-    template <typename... Args>
-    inline void subscribe(StreamType& stream, const uint8_t index, Args&... args)
-    {
-        Packetizer::subscribe(stream, index, [&](const uint8_t* data, const uint8_t size)
-        {
-            MsgPack::Unpacker unpacker;
-            unpacker.feed(data, size);
-            unpacker.decode(args...);
-        });
-    }
-
-#ifndef HT_SERIAL_MSGPACKETIZER_DISABLE_STL
-
-    namespace detail
-    {
-        template <typename R, typename... Args>
-        inline void subscribe(StreamType& stream, const uint8_t index, const std::function<R(Args...)>& callback)
-        {
-            Packetizer::subscribe(stream, index, [callback](const uint8_t* data, const uint8_t size)
-            {
-                MsgPack::Unpacker unpacker;
-                unpacker.feed(data, size);
-                std::tuple<std::remove_cvref_t<Args>...> t;
-                unpacker.decodeTo(t);
-                std::apply(callback, t);
-            });
-        }
-
-        template <typename R, typename... Args>
-        inline void subscribe(StreamType& stream, const std::function<R(Args...)>& callback)
-        {
-            Packetizer::subscribe(stream, [callback](const uint8_t index, const uint8_t* data, const uint8_t size)
-            {
-                MsgPack::Unpacker unpacker;
-                unpacker.feed(data, size);
-                callback(index, unpacker);
-            });
-        }
-    }
-
-    template <typename F>
-    inline auto subscribe(StreamType& stream, const uint8_t index, const F& callback)
-    -> std::enable_if_t<arx::is_callable<F>::value>
-    {
-        detail::subscribe(stream, index, arx::function_traits<F>::cast(callback));
-    }
-
-    template <typename F>
-    inline auto subscribe(StreamType& stream, const F& callback)
-    -> std::enable_if_t<arx::is_callable<F>::value>
-    {
-        detail::subscribe(stream, arx::function_traits<F>::cast(callback));
-    }
-
+#ifdef HT_SERIAL_MSGPACKETIZER_DISABLE_STL
+    using DecoderRef = arx::shared_ptr<MsgPack::Unpacker>;
+    using DecoderMap = arx::map<StreamType*, DecoderRef, PACKETIZER_MAX_STREAM_MAP_SIZE>;
+    using namespace arx;
+#else
+    using DecoderRef = std::shared_ptr<MsgPack::Unpacker>;
+    using DecoderMap = std::map<StreamType*, DecoderRef>;
+    using namespace std;
 #endif // HT_SERIAL_MSGPACKETIZER_DISABLE_STL
+
+
+    class EncodeManager
+    {
+        EncodeManager() {}
+        EncodeManager(const EncodeManager&) = delete;
+        EncodeManager& operator=(const EncodeManager&) = delete;
+
+        MsgPack::Packer encoder;
+
+    public:
+
+        static EncodeManager& getInstance()
+        {
+            static EncodeManager m;
+            return m;
+        }
+
+        MsgPack::Packer& getEncoder()
+        {
+            return encoder;
+        }
+    };
 
     template <typename... Args>
     inline void send(StreamType& stream, const uint8_t index, Args&&... args)
     {
-        MsgPack::Packer packer;
+        auto& packer = EncodeManager::getInstance().getEncoder();
+        packer.clear();
         packer.encode(std::forward<Args>(args)...);
         Packetizer::send(stream, index, packer.data(), packer.size());
     }
 
     inline void send(StreamType& stream, const uint8_t index, const uint8_t* data, const uint8_t size)
     {
-        MsgPack::Packer packer;
+        auto& packer = EncodeManager::getInstance().getEncoder();
+        packer.clear();
         packer.encode(data, size);
         Packetizer::send(stream, index, packer.data(), packer.size());
     }
+
+
+    class DecodeManager
+    {
+        DecodeManager() {}
+        DecodeManager(const DecodeManager&) = delete;
+        DecodeManager& operator=(const DecodeManager&) = delete;
+
+        DecoderMap decoders;
+
+    public:
+
+        static DecodeManager& getInstance()
+        {
+            static DecodeManager m;
+            return m;
+        }
+
+        DecoderRef getDecoderRef(const StreamType& stream)
+        {
+            StreamType* s = (StreamType*)&stream;
+            if (decoders.find(s) == decoders.end())
+                decoders.insert(make_pair(s, make_shared<MsgPack::Unpacker>()));
+            return decoders[s];
+        }
+
+        const DecoderMap& getDecoderMap() const
+        {
+            return decoders;
+        }
+
+        DecoderMap& getDecoderMap()
+        {
+            return decoders;
+        }
+    };
+
+
+    template <typename... Args>
+    inline void subscribe(StreamType& stream, const uint8_t index, Args&... args)
+    {
+        Packetizer::subscribe(stream, index, [&](const uint8_t* data, const uint8_t size)
+        {
+            auto unpacker = DecodeManager::getInstance().getDecoderRef(stream);
+            unpacker->clear();
+            unpacker->feed(data, size);
+            unpacker->decode(args...);
+        });
+    }
+
+    namespace detail
+    {
+        template <typename R, typename... Args>
+        inline void subscribe(StreamType& stream, const uint8_t index, std::function<R(Args...)>&& callback)
+        {
+            Packetizer::subscribe(stream, index,
+                [&, cb {std::move(callback)}](const uint8_t* data, const uint8_t size)
+                {
+                    auto unpacker = DecodeManager::getInstance().getDecoderRef(stream);
+                    unpacker->clear();
+                    unpacker->feed(data, size);
+                    std::tuple<std::remove_cvref_t<Args>...> t;
+                    unpacker->decodeTo(t);
+                    std::apply(cb, t);
+                }
+            );
+        }
+
+        template <typename R, typename... Args>
+        inline void subscribe(StreamType& stream, std::function<R(Args...)>&& callback)
+        {
+            Packetizer::subscribe(stream,
+                [&, cb {std::move(callback)}](const uint8_t index, const uint8_t* data, const uint8_t size)
+                {
+                    auto unpacker = DecodeManager::getInstance().getDecoderRef(stream);
+                    unpacker->clear();
+                    unpacker->feed(data, size);
+                    cb(index, *unpacker);
+                }
+            );
+        }
+    }
+
+    template <typename F>
+    inline auto subscribe(StreamType& stream, const uint8_t index, F&& callback)
+    -> std::enable_if_t<arx::is_callable<F>::value>
+    {
+        detail::subscribe(stream, index, arx::function_traits<F>::cast(std::move(callback)));
+    }
+
+    template <typename F>
+    inline auto subscribe(StreamType& stream, F&& callback)
+    -> std::enable_if_t<arx::is_callable<F>::value>
+    {
+        detail::subscribe(stream, arx::function_traits<F>::cast(std::move(callback)));
+    }
+
 
     inline void parse(bool b_exec_cb = true)
     {
